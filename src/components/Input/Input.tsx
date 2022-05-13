bb553201960af9ddaf66498041b26c7124ab84de
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2022-05-13 17:08:37 -0300

    feat: finished register election page

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,11 +4,14 @@ type InputData = {
     type: string, 
     name: string,
     handleCandidateData?: (name : string, event : any) => void
+    handleInputData?: (field: string, event: any) => void
 }
 
-const Input = ({ type, name, handleCandidateData } : InputData) => {
+const Input = ({ type, name, handleCandidateData, handleInputData } : InputData) => {
     return(
-        <input type={type} name={name} className={styles.input} onChange={handleCandidateData !== undefined ? (event) => handleCandidateData(name, event) : (event) => event.preventDefault()} />
+        // <input type={type} name={name} className={styles.input} onChange={handleCandidateData !== undefined ? (event) => handleCandidateData(name, event) : (event) => event.preventDefault()} />
+
+        <input type={type} name={name} className={styles.input} onChange={handleCandidateData !== undefined ? (event) => handleCandidateData(name, event) : (handleInputData !== undefined ? (event) => handleInputData(name, event) : (event) => event.preventDefault())} />
     )
 }
 
--- src/components/RegisterElection/RegisterElection.tsx
+++ src/components/RegisterElection/RegisterElection.tsx
@@ -1,9 +1,10 @@
-import { VStack, Button, Heading, Box, Image, FormControl, FormLabel, Text, Checkbox, HStack } from "@chakra-ui/react"
+import { VStack, Button, Heading, Box, Image, FormControl, FormLabel, Text, Checkbox, HStack, Alert, AlertIcon } from "@chakra-ui/react"
 import Input from "../Input/Input"
-import { useEffect, useState, useRef } from "react"
+import { useEffect, useState } from "react"
 import api from "../../api/api"
 
 import laptop from "../../assets/laptop.png"
+import { stringify } from "querystring"
 
 
  
@@ -11,6 +12,9 @@ import laptop from "../../assets/laptop.png"
 const RegisterElection = () => {
     const [ availableCandidates, setAvailableCandidates ] = useState<any>([])
     const [ electionsCandidates, setElectionsCandidates ] = useState<any>([])
+    const [ showAlert, setShowAlert ] = useState<boolean>(false)
+    const [ date, setDate ] = useState<string>("")
+    const [ electionTitle, setElectionTitle ] = useState<string>("")
 
     useEffect(() => {
         async function getCandidates() {
@@ -22,41 +26,120 @@ const RegisterElection = () => {
         getCandidates()
     }, [])
 
-    async function handleElectionRegistration() {
-        
-        const electionTitleRef = useRef()
-        const durationDate = useRef()
-        
+    function handleInputData(field : string, event : any, ) {
+        event.preventDefault() 
+
+        if(field === "electionTitle") setElectionTitle(event.target.value)
+        if(field === "date") setDate(event.target.value)
     }
 
-    return(
-        <VStack align="center" direction="column" gap="0.7rem">
-            <Box boxSize="7rem" marginTop="4rem">
-                <Image src={laptop} alt="silver laptop" />
-            </Box>
+    function getCheckboxValue(event : any) {  
+        event.preventDefault()
 
-            <Heading>Coletando dados...</Heading>
+        
+        let candidateJSON = {
+            "name": event.target.value,
+            "id": event.target.id
+        }
 
-            <FormControl width="fit-content">
-                <FormLabel fontWeight="bold" htmlFor="electionTitle">Qual o nome do pleito?</FormLabel>
-                <Input type="text" name="electionTitle"></Input>
-            </FormControl>
+        if(event.target.checked) {
+            event.target.isChecked = true 
+            setElectionsCandidates([...electionsCandidates, candidateJSON])
+        } else {
+            const candidateToBeRemoved = electionsCandidates.filter((candidate : any) => {
+                if(candidate.id === event.target.id) {
+                   return electionsCandidates.indexOf(candidate)  
+                }
+            })
 
-            <FormControl width="fit-content">
-                <FormLabel fontWeight="bold" htmlFor="date">Data inicial e final:</FormLabel>
-                <Input type="text" name="date"></Input>
-            </FormControl>
+            electionsCandidates.splice(candidateToBeRemoved, 1)
+        }
+    }
 
-            <Text fontWeight="bold">Quais são os candidatos?</Text>
-            <HStack>
-                { availableCandidates !== [] ? availableCandidates.map((candidate : any) => {
-                    return <Checkbox name="candidates" size="md" key={candidate.id} >{candidate.name}</Checkbox>
-                }) : null }
-            </HStack>
+    async function handleElectionRegistrationForm(event : any) {
+        event.preventDefault()
 
-            <Button bg="green.200" color="#FFF" _hover={{ color: "white", background: "green.400" }}>Cadastrar</Button>
-        </VStack>
-    )
+        await api.post("/elections", {
+            title: electionTitle,
+            date: date,
+            amountOfCandidates: electionsCandidates.length,
+            candidates: electionsCandidates
+        }).then(response => {
+            if(response) {
+                setShowAlert(true)
+
+                setTimeout(() => {
+                    setShowAlert(false)
+                }, 3000)
+            }
+        })
+    }
+
+    if(showAlert) {
+        return(
+            <VStack align="center" direction="column" gap="0.7rem">
+                <Alert status="success" variant="top-accent" >
+                    <AlertIcon />
+                    Eleição registrada!
+                </Alert>
+
+                <Box boxSize="7rem" marginTop="4rem">
+                    <Image src={laptop} alt="silver laptop" />
+                </Box>
+    
+                <Heading>Coletando dados...</Heading>
+    
+                <FormControl width="fit-content">
+                    <FormLabel fontWeight="bold" htmlFor="electionTitle">Qual o nome do pleito?</FormLabel>
+                    <Input handleInputData={handleInputData} type="text" name="electionTitle" />
+                </FormControl>
+    
+                <FormControl width="fit-content">
+                    <FormLabel fontWeight="bold" htmlFor="date">Data inicial e final:</FormLabel>
+                    <Input handleInputData={handleInputData} type="text" name="date" />
+                </FormControl>
+    
+                <Text fontWeight="bold">Quais são os candidatos?</Text>
+                <HStack>
+                    { availableCandidates !== [] ? availableCandidates.map((candidate : any) => {
+                        return <Checkbox name="candidate" size="md" key={candidate.id} value={candidate.name} onChange={event => getCheckboxValue(event)} id={candidate.id}>{candidate.name}</Checkbox>
+                    }) : null }
+                </HStack>
+    
+                <Button bg="green.200" color="#FFF" _hover={{ color: "white", background: "green.400" }} onClick={event => handleElectionRegistrationForm(event)}>Cadastrar</Button>
+            </VStack>
+        )
+
+    } else {
+        return(
+            <VStack align="center" direction="column" gap="0.7rem">
+                <Box boxSize="7rem" marginTop="4rem">
+                    <Image src={laptop} alt="silver laptop" />
+                </Box>
+    
+                <Heading>Coletando dados...</Heading>
+    
+                <FormControl width="fit-content">
+                    <FormLabel fontWeight="bold" htmlFor="electionTitle">Qual o nome do pleito?</FormLabel>
+                    <Input handleInputData={handleInputData} type="text" name="electionTitle" />
+                </FormControl>
+    
+                <FormControl width="fit-content">
+                    <FormLabel fontWeight="bold" htmlFor="date">Data inicial e final:</FormLabel>
+                    <Input handleInputData={handleInputData} type="text" name="date" />
+                </FormControl>
+    
+                <Text fontWeight="bold">Quais são os candidatos?</Text>
+                <HStack>
+                    { availableCandidates !== [] ? availableCandidates.map((candidate : any) => {
+                        return <Checkbox name="candidate" size="md" key={candidate.id} value={candidate.name} onChange={event => getCheckboxValue(event)} id={candidate.id}>{candidate.name}</Checkbox>
+                    }) : null }
+                </HStack>
+    
+                <Button bg="green.200" color="#FFF" _hover={{ color: "white", background: "green.400" }} onClick={event => handleElectionRegistrationForm(event)}>Cadastrar</Button>
+            </VStack>
+        )
+    }
 }
 
 export default RegisterElection 
